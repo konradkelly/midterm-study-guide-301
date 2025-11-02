7b4e2c64c7875f2e6a5ab886a2b028a1c81c5390
konradkelly <konradky@gmail.com>
2025-11-02 10:27:20 -0800

    implement maxDiff method

--- src/Practice.java
+++ src/Practice.java
@@ -5,15 +5,28 @@ public class Practice {
      * @param nums a non-empty, non-null array of numbers
      * @return the difference between the largest and smallest number
      */
-    public static int maxDiff(int[] nums) {
-        // TODO: implement this
-        return -1;
-    }
+        public static int maxDiff(int[] nums) {
+            if (nums.length == 0) {
+                return 0;
+            }
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] > max) {
+                    max = nums[i];
+                }
+                if (nums[i] < min) {
+                    min = nums[i];
+                }
+            }
 
-
+            return max - min;
+        }     
+    }  
     // TODO: Implement the other methods from the study guide AND tests for each one
 
     // For each method you are only required to implement it for one of the data
     // structures. But use a different data structure for each method. For example,
     // do maxDiff with an array, the next question with a Set, etc.
-}
+
