37d3264c474d6bfc0e0bf94f48f565347a725675
konradkelly <konradky@gmail.com>
2025-11-02 23:30:57 -0800

    change data structures for findLongestCWord and inBetweenNandM methods

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Practice {
     /**
      * Returns the difference between the largest and smallest integer in an array.
@@ -25,8 +29,8 @@ public class Practice {
         }     
     
     
-        public static String findLongestCWord(String[] words) {
-            if (words.length == 0) {
+        public static String findLongestCWord(ArrayList<String> words) {
+            if (words.isEmpty()) {
                 return "";
             }
             String longest = "";
@@ -37,11 +41,11 @@ public class Practice {
                     }
                 }
             }
-                return longest;
+            return longest;
         }
             
-        public static int inBetweenNandM(String[] words, int n, int m) {
-                if (words.length == 0) {
+        public static int inBetweenNandM(HashSet<String> words, int n, int m) {
+                if (words.isEmpty()) {
                     return 0;
                 }
                 int count = 0;
@@ -51,42 +55,8 @@ public class Practice {
                 }
             }
                 return count;
-        }
-    
-    
-        public static int evenOddDiff(int[] arr) {
-            int evenCount = 0;
-            int oddCount = 0;
-
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] % 2 == 0) {
-                    evenCount++;
-                } else if (arr[i] % 2 != 0) {
-                    oddCount++;
-                    }
-                }
-                return evenCount - oddCount;
-            }
-        }
-        public static int findSecondLargest(int arr[]) {
-                if (arr.length == 0) {
-                    return 0;
-                }
-                int largest = Integer.MIN_VALUE;
-                int secondLargest = Integer.MIN_VALUE;
-
-                for (int i = 0; i < arr.length; i++) {
-                    if (arr[i] > largest) {
-                        secondLargest = largest;
-                        largest = arr[i];
-                    } else if (arr[i] > secondLargest && arr[i] < largest) {
-                        secondLargest = arr[i];
-                    }
-                }
-                return secondLargest; 
-            }                          
-    // TODO: Implement the other methods from the study guide AND tests for each one
-
+        }                
+    }
     // For each method you are only required to implement it for one of the data
     // structures. But use a different data structure for each method. For example,
     // do maxDiff with an array, the next question with a Set, etc.
